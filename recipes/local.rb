fbc90c408f69fabb20c1d678179e0e5f85d2a09c
Jonathon W. Marshall <jonathon@bnotions.com>
2013-09-04 17:55:14 -0400

    merge celery into local

--- recipes/local.rb
+++ recipes/local.rb
@@ -9,24 +9,36 @@
 
 include_recipe "twoscoops::base"
 include_recipe "twoscoops::database"
+include_recipe "rabbitmq"
+include_recipe "supervisor"
 
-directory "/vagrant/logs" do
+user "celery"
+
+directory "/var/run/celery" do
+  owner "celery"
+end
+
+directory "/var/lib/celery" do
+  owner "celery"
+end
+
+directory "#{node['twoscoops']['application_path']}/logs" do
   action :create
   mode 00755
 end
 
-template "/vagrant/#{node['twoscoops']['project_name']}/#{node['twoscoops']['project_name']}/settings/database.py" do
+template "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}/#{node['twoscoops']['project_name']}/settings/database.py" do
   source "database.py.erb"
   mode 00644
 end
 
 execute "pip-install-requirements" do
-  cwd "/vagrant"
+  cwd "#{node['twoscoops']['application_path']}"
   command "pip install -r requirements/local.txt"
 end
 
 execute "django-syncdb" do
-  cwd "/vagrant/#{node['twoscoops']['project_name']}"
+  cwd "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
   command "python manage.py syncdb --noinput"
 end
 
@@ -41,23 +53,51 @@ template "/tmp/twoscoops/fixtures/createsuperuser.json" do
 end
 
 execute "django-createsuperuser" do
-  cwd "/vagrant/#{node['twoscoops']['project_name']}"
+  cwd "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
   command "python manage.py loaddata /tmp/twoscoops/fixtures/createsuperuser.json"
 end
 
 execute "django-migrate" do
-  cwd "/vagrant/#{node['twoscoops']['project_name']}"
+  cwd "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
   command "python manage.py migrate"
 end
-
-include_recipe "supervisor"
 
 supervisor_service "django" do
   command "python manage.py runserver 0.0.0.0:8080"
   autostart true
-  directory "/vagrant/#{node['twoscoops']['project_name']}"
-  stdout_logfile "/vagrant/logs/django.log"
-  stderr_logfile "/vagrant/logs/django_error.log"
+  directory "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
+  stdout_logfile "#{node['twoscoops']['application_path']}/logs/django.log"
+  stderr_logfile "#{node['twoscoops']['application_path']}/logs/django_error.log"
+  action :enable
+end
+
+supervisor_service "celeryd" do
+  command "python manage.py celeryd --loglevel=INFO"
+  user "celery"
+  autostart true
+  directory "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
+  stdout_logfile "#{node['twoscoops']['application_path']}/logs/celeryd.log"
+  stderr_logfile "#{node['twoscoops']['application_path']}/logs/celeryd_error.log"
+  action :enable
+end
+
+supervisor_service "celery-worker" do
+  command "python manage.py celery worker --loglevel=INFO"
+  user "celery"
+  autostart true
+  directory "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
+  stdout_logfile "#{node['twoscoops']['application_path']}/logs/celery_worker.log"
+  stderr_logfile "#{node['twoscoops']['application_path']}/logs/celery_worker_error.log"
+  action :enable
+end
+
+supervisor_service "celery-beat" do
+  command "python manage.py celery beat --pidfile=/var/run/celery/celery-beat.pid --schedule=/var/lib/celery/celerybeat-schedule --loglevel=INFO"
+  user "celery"
+  autostart true
+  directory "#{node['twoscoops']['application_path']}/#{node['twoscoops']['project_name']}"
+  stdout_logfile "#{node['twoscoops']['application_path']}/logs/celery_beat.log"
+  stderr_logfile "#{node['twoscoops']['application_path']}/logs/celery_beat_error.log"
   action :enable
 end
 
